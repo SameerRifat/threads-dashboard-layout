68ed5357291537175968f468c17e1c2acba9fb25
Sameer <sameerrifatbrw789@gmail.com>
2024-02-05 18:08:48 +0500

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main className="h-screen">
+      {/* <UserButton afterSignOutUrl="/"/> */}
+        <h1 className="head-text text-left">Home</h1>
+    </main>
+  );
+}
